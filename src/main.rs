fa4958a1a9a7dd8079cb0fa611118e97572615e3
nuttyCream <issanutty@gmail.com>
2025-04-19 15:43:17 -0700

    feat: add skelly

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, error::Error, net::SocketAddr};
+
+use axum::{Router, response::IntoResponse, routing::get};
+use listenfd::ListenFd;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let port = env::args()
+        .nth(1)
+        .and_then(|arg| arg.parse::<i32>().ok())
+        .unwrap_or(3000);
+
+    let addr = format!("0.0.0.0:{port}");
+    let app = Router::new().route("/", get(serve_html));
+
+    let mut listenfd = ListenFd::from_env();
+    let listener = match listenfd.take_tcp_listener(0)? {
+        Some(listener) => {
+            listener.set_nonblocking(true)?;
+            TcpListener::from_std(listener)?
+        }
+        None => TcpListener::bind(addr).await?,
+    };
+
+    let shutdown = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("failed to get C-c signhandle");
+    };
+
+    println!("listening on {}", listener.local_addr()?);
+    axum::serve(
+        listener,
+        app.into_make_service_with_connect_info::<SocketAddr>(),
+    )
+    .with_graceful_shutdown(shutdown)
+    .await?;
+
+    Ok(())
 }
+
+async fn serve_html() -> impl IntoResponse {}
