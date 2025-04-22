d90fb078b510214a728d782fcd91bf8831bfd229
nuttyCream <issanutty@gmail.com>
2025-04-22 10:47:53 -0700

    feat: impl shared mem skeeely

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,36 @@ use axum::{
 };
 use futures::{SinkExt, StreamExt};
 use listenfd::ListenFd;
+use shared_memory::{Shmem, ShmemConf};
 use tokio::net::TcpListener;
+
+//https://github.com/elast0ny/shared_memory/blob/master/examples
+
+#[repr(C)]
+#[derive(Debug, Clone, Copy)]
+struct Shared {
+    ver: i32,
+    direction: i32,
+    motor_power: [i32; 3],
+}
+
+// should this be open
+// the entire time web server
+// is running??
+fn open_shared_mem() -> Result<Shmem, Box<dyn Error>> {
+    let mem = ShmemConf::new()
+        .os_id("omnigod")
+        .size(size_of::<Shared>())
+        .open()?;
+
+    Ok(mem)
+}
+
+fn read_shared_mem(mem: &Shmem) -> Shared {
+    let ptr = mem.as_ptr() as *const Shared;
+    // unsafe { read_volatile(ptr) }
+    unsafe { ptr.read() }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -22,11 +51,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .nth(1)
         .and_then(|arg| arg.parse::<i32>().ok())
         .unwrap_or(3000);
-
     let addr = format!("0.0.0.0:{port}");
+
     let app = Router::new()
         .route("/", get(serve_html))
         .route("/style.css", get(serve_css))
+        .route("/htmx.js", get(serve_js))
         .route("/ws", any(handle_websocket));
 
     let mut listenfd = ListenFd::from_env();
@@ -72,6 +102,17 @@ async fn handle_socket(socket: WebSocket) {
             .is_err()
         {}
     });
+
+    let shared = match open_shared_mem() {
+        Ok(mem) => {
+            println!("opened shared mem");
+            mem
+        }
+        Err(e) => {
+            println!("failed to open shared mem: {}", e);
+            return;
+        }
+    };
 
     let mut recv_task = tokio::spawn(async move {
         while let Some(Ok(msg)) = receiver.next().await {
@@ -134,4 +175,9 @@ async fn serve_html() -> Html<&'static str> {
 async fn serve_css() -> impl IntoResponse {
     let css = include_str!("./assets/style.css");
     ([(header::CONTENT_TYPE, "text/css")], css)
+}
+
+async fn serve_js() -> impl IntoResponse {
+    let js = include_str!("./assets/htmx.js");
+    ([(header::CONTENT_TYPE, "application/javascript")], js)
 }
